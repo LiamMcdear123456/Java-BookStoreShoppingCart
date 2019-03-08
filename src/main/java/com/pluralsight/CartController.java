666a0e733bc48a8f3cd9d172c24f56905251c1f6
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-03-08 09:57:48 +0000

    Update working

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -44,9 +44,12 @@ public class CartController extends HttpServlet {
 			switch(action) {
 				case "/addcart":
 					 addToCart(request, response);
-           break;
+					 break;
 				case "/delete":
 					deleteFromCart(request, response);
+					break;
+				case "/update":
+					updateCart(request, response);
 					break;
         default:
            break;
@@ -64,6 +67,15 @@ public class CartController extends HttpServlet {
 		int index = Integer.parseInt(request.getParameter("index"));
 		ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
 		cart.deleteCartItem(index);
+	}
+	
+	private void updateCart(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int index = Integer.parseInt(request.getParameter("index"));
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
+		cart.updateCartItem(index, quantity);
 	}
 
   protected void addToCart(HttpServletRequest request, HttpServletResponse response)
